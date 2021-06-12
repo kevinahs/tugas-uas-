20f0352dc2d0bb08df129ca4e8145fd57a4aee6b
kevinahs <85783397+kevinahs@users.noreply.github.com>
2021-06-12 15:58:43 +0700

    Tugas UAS 190402164 Kevin August Hizkia S

---
+++ tugas uas.py
@@ -0,0 +1,66 @@
+#uas pemrograman komputer
+
+#1
+a= "kevin"
+b= "august"
+
+print(a,b)
+
+#2
+a= 19
+b= "elektro"
+
+print(a,b)
+
+#3
+a= "kevin, august"
+
+print(a[1])
+
+#4
+a= "kevin, august"
+
+print(len(a))
+
+#5
+a= 15
+b= 15
+
+if a > b:
+    print("a lebih besar dari b")
+elif a == b:
+    print("a dan b adalah sama")
+else:
+    print("a lebih kecil dari b")
+
+#6
+a= 1000
+b= 999.9
+
+if a > b:
+    print("a lebih besar dari b")
+elif a == b:
+    print("a dan b adalah sama")
+else:
+    print("a lebih kecil dari b")
+
+#7
+a= 999.9
+b= 1000
+
+if a > b:
+    print("a lebih besar dari b")
+elif a == b:
+    print("a dan b adalah sama")
+else:
+    print("a lebih kecil dari b")
+
+#8
+listyangakandibeli= ["pulpen", "buku tulis", "buku gambar"]
+print(listyangakandibeli)
+#9
+todolist= ["nyapu", "ngepel", "nyuci piring"]
+print(todolist)
+#10
+txt= "aku ganteng"
+print("ganteng" in txt)
